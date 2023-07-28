5d0c95985012bd0536a4909d01b7c51781aafd0a
soon <sudosoon@gmail.com>
2023-07-28 12:01:33 +0900

    init

---
+++ soon-ssh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function typeHostName() {
+  echo ""
+  echo -n "HostName: "
+  read hostName
+  host=$(sed -n s/^#${hostName}=//p ./conf)
+  if [ "${#host}" -gt 0 ]
+    then
+      if [ -f ./keys/$hostName.key ]
+        then
+          echo ""
+          echo "Connet To $host."
+          echo ""
+          ssh -i ./keys/$hostName.key $host 
+        else
+          echo "fail: Key file not found. Please re-enter HostName."
+          typeHostName
+        fi
+    else
+      echo "fail: HostName does not exist. Please re-enter HostName."
+      typeHostName
+  fi
+}
+echo -e "                                                        __         \n                                                       /\ \        \n  ____    ___     ___     ___              ____    ____\ \ \___    \n /',__\  / __\`\  / __\`\ /' _ \`\  _______  /',__\  /',__\\ \  _ \`\  \n/\__, \`\/\ \L\ \/\ \L\ \/\ \/\ \/\______\/\__, \`\/\__, \`\\ \ \ \ \ \n\/\____/\ \____/\ \____/\ \_\ \_\/______/\/\____/\/\____/\ \_\ \_\ \n \/___/  \/___/  \/___/  \/_/\/_/         \/___/  \/___/  \/_/\/_/\n                                                                   "
+typeHostName
